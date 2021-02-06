5249714579e4526be0ffaf3a6185b7f150173b1d
Victoria Olivieri <vro6068@rit.edu>
2021-02-05 23:02:15 -0500

    Start of PE3

---
+++ Olivieri_PE3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olivieri_PE3
+{
+    // Class Program
+    // Author: Victoria Olivieri
+    // Purpose: PE3 - Variables and Expressions
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: 
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            //1. In the following code, how would we refer 
+            // to the name great from code in the namespace fabulous?
+
+            //2. Which of the following are not legal variable names.  Why?
+            //myVariableIsGood --> Legal = variable begins with a letter and has no spaces
+            //99Flake --> illegal = cannot start with a number
+            //iLike#Tags --> illegal = Can only contain _, numbers or letters. # cannot be used
+            //_floor --> legal = begins with _ and contains letter
+            //rit_tigers --> legal = begins with a letter and contains _ instead of a space
+            //time2GetJiggyWidIt -->Legal = begins with a letter and contain letters (although its a bit long)
+            //$bankBalance --> illegal = Cannot contian $. Also can only begin with a letter, _, or @
+            //Factorial! -->
+            //MAX_SIZE -->
+            //black & white --> illegal = Cannot have spaces
+
+            //3.	Is the string "supercalifragilisticexpialidocious" 
+            //too big to fit in a string variable? Why?
+            
+
+
+        }
+    }
+}
