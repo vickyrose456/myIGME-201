55e47ca786b01420e392fbe9cff3dda1782a28ca
Victoria Olivieri <vro6068@rit.edu>
2021-02-10 20:34:22 -0500

    Beginning comments

---
+++ Olivieri_PE6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olivieri_PE6
+{
+    // Class Program
+    // Author: Victoria Olivieri
+    // Purpose: PE6 - Parsing and Formatting 
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: Create a simple number guessing game. It will generate
+        //a random int b/w 0 and 100 and a player will try to guess it.
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
