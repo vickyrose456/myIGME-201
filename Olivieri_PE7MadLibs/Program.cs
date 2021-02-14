d5e55182e6d313b632762817f9f2dfdcb0f33ff9
Victoria Olivieri <vro6068@rit.edu>
2021-02-13 21:45:55 -0500

    reading txt file

--- Olivieri_PE7MadLibs/Program.cs
+++ Olivieri_PE7MadLibs/Program.cs
@@ -3,19 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Olivieri_PE7MadLibs
 {
-    //PE7 - Mad Libs
+    //Class PE7 - Mad Libs
     //Author: Victoria Olivieri
     //Purpose: Practice working with strings, lists, and input/output to create a madlibs game. 
     //using predetermined narratives stored in a txt file, gather info from user to generate a story. 
     //Restrictions: None.
     class Program
     {
-
+        // Method: Main
+        // Purpose: 
+        // Restrictions: None
         static void Main(string[] args)
         {
+
+            StreamReader txtInput = null;
+
+            try
+            {
+                txtInput = new StreamReader("c:\\templates\\MadLibsTemplate.txt");
+                String line = txtInput.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error with file: " + e.Message);
+            }
+            finally 
+            {
+                if (txtInput != null)
+                {
+                    txtInput.Close();
+                }
+            }
+
+
+
+
 
         }
     }
