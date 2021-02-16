6de1c4d7b40d0607aed756efe3bb6fcebf639d3f
Victoria Olivieri <vro6068@rit.edu>
2021-02-15 23:13:16 -0500

    PE Question 8

---
+++ Olivieri_PE8Question8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olivieri_PE8Question8
+{
+    //Class PE8 - Question 
+    //Author: Victoria Olivieri
+    //Purpose: write an application that accepts a string and does case senstive replacement
+    //Restrictions: None.
+    class Program
+    {
+        // Method: Main
+        // Purpose: The user will input a sentence. Case sensitive - replace the word
+        //no with the word yes
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            string userIn = null;
+
+            //get the input from the user
+            Console.WriteLine("Enter a phrase using the word \"yes\" or \"no\"");
+            userIn = Convert.ToString(Console.ReadLine());
+        
+        }
+    }
+}
