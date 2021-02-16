9d26b1d9ca0cda4edce7057f72bc045340ea4a79
Victoria Olivieri <vro6068@rit.edu>
2021-02-15 22:19:17 -0500

    Question 8 start

---
+++ Olivieri_PE8_Question7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olivieri_PE8_Question7
+{
+    //Class PE8 - Question 7
+    //Author: Victoria Olivieri
+    //Purpose: Take user input as a string and output it in reverse order
+    //Restrictions: None.
+    class Program
+    {
+        // Method: Main
+        // Purpose: Accept a string from the user and outputs a string w/ chars in reverse order
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            string userIn = null;
+            string revInput = null;
+
+            //get user input
+            Console.WriteLine("Enter a word");
+            userIn = Console.ReadLine();
+
+
+        }
+    }
+}
