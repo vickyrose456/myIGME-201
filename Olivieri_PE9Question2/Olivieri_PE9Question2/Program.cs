b576d2efcd1155852b7f03e96b4cde8843c07d87
Victoria Olivieri <vro6068@rit.edu>
2021-02-17 14:49:18 -0500

    factorial beginning

---
+++ Olivieri_PE9Question2/Olivieri_PE9Question2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olivieri_PE9Question2
+{
+    //Class PE9 - Question 2
+    //Author: Victoria Olivieri
+    //Purpose:  Add a timer to the Math Quiz Solution to elapse in 5 seconds for
+    //each question, and mark the answer wrong if the timer expires
+    //Restrictions: None.
+    class Program
+    {
+        // Method: Main
+        // Purpose: The user will be asked math questions within a 5 second time limit
+        //if they do not get the answeer right, then it is incorrect
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ factorial/factorial/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sNumber = null;
+            int nNumber = 0;
+            int nAnswer = 0;
+
+
+            do
+            {
+                Console.Write("Enter positiv number: ");
+                sNumber = Console.ReadLine();
+            } while (!int.TryParse(sNumber, out nNumber) && nNumber <= 0);
+
+            //nswer = 1;
+            /*
+            while (nNumber > 0)
+            {
+                nAnswer *= nNumber;
+                --nNumber;
+            }
+            */
+
+
+            //N * (N-1) * (N -2) * (N -3)....
+            //fact of n = cyclic loop
+            //every recursive method has a base case
+
+
+
+            nAnswer = Factorial(nNumber);
+
+        }//end main
+
+        static int Factorial(int v)
+        {
+            int returnVal = 0;
+            int nextVal = 0;
+
+            if (v == 0)
+            {
+                returnVal = 1;
+            }
+            else if (v < 0)
+            {
+                returnVal = -1;
+            }
+            else 
+            {
+                //fact of nnxt sequence
+                nextVal = Factorial(v - 1);
+
+                returnVal = v * nextVal;
+            }
+
+
+        }
+
+    }
+}
