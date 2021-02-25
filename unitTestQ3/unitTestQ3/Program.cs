abc516e5761fc6f27b94833bfacdb2b225d44a67
Victoria Olivieri <vro6068@rit.edu>
2021-02-24 23:29:05 -0500

    Completed Q3- Delegates to read line

---
+++ unitTestQ3/unitTestQ3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unitTestQ3
+{
+    //class: Unit test question 3
+    //Author: Victoria Olivieri
+    //Purpose: Create a console application that uses
+    //a delegate to impersonate the Console.ReadLine() function when asking for user input
+    //Restrictions: None.
+    class Program
+    {
+        //declare delegate function
+        delegate string MyReadLine();
+
+
+        // Method: Main
+        // Purpose: Use delegates to impersonate readline function
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            //create a var to call fns
+            MyReadLine myReadLine;
+
+            //construct delegate fns ref to point to console.readline
+            myReadLine = new MyReadLine(Console.ReadLine);
+
+            //call the new fns
+            string readString = myReadLine();
+
+        }
+    }
+}
