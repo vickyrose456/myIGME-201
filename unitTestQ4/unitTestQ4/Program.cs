f5a67634a97760cd4bcd40cd6fbdc7393ada22fd
Victoria Olivieri <vro6068@rit.edu>
2021-02-25 00:35:44 -0500

    Start of Q4. Got user input

--- unitTestQ4/unitTestQ4/Program.cs
+++ unitTestQ4/unitTestQ4/Program.cs
@@ -13,15 +13,47 @@ namespace unitTestQ4
     class Program
     {
         // Method: Main
-        // Purpose: 
+        // Purpose: Allow the user to choose b/w 3 questions. The user has
+        // 5 seconds to answer the question correctly. If they answer incorrectly
+        // then the correct answer is written 
         // Restrictions: None
         static void Main(string[] args)
         {
+            bool bValid = false;
+
+            //var for user question num
+            int qNum = 0;
+            //var for user input 
+            string userIn = null;
 
 
         //the start of the game asks the user what question they want
         start:
-            Console.WriteLine("Which question would you like? (1-3) ");
+
+            while (!bValid)
+            {
+                //ask user for input
+                Console.WriteLine("Which question would you like? (1-3) ");
+               
+                try
+                {
+                    //read the input into qNum for the num question
+                    qNum = int.Parse(Console.ReadLine());
+                    //if the number is not 1, 2 or 3 then ask  for new user input
+                    while (qNum < 1 ^ qNum > 3)
+                    {
+                        //get new input until it is 1 2 or 3
+                        Console.WriteLine("Which question would you like? (1-3) ");
+                        qNum = int.Parse(Console.ReadLine());
+                    }
+                    bValid = true;
+                }catch
+                {
+                    //do nothing
+                    bValid = false;
+                }
+            }
+            
 
 
 
