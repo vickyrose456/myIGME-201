4d3eedaae212a8eb0dbfec53cc7451b7d7e744b8
Victoria Olivieri <vro6068@rit.edu>
2021-02-26 23:10:45 -0500

    Founded errors. Program will calculate the power

--- unitTestQ14/unitTestQ14/Program.cs
+++ unitTestQ14/unitTestQ14/Program.cs
@@ -30,33 +30,38 @@ namespace UT1_BugSquash
                 //need to assign this input to nY(need to convert from string to int)
                 nY = Convert.ToInt32(Console.ReadLine());
             } //while (int.TryParse(sNumber, out nX));
-            while (int.TryParse(sNumber, out nX));//runtime error - need to put !, otherwise it will continue 
+            while (!int.TryParse(sNumber, out nX));//runtime error - need to put !, otherwise it will continue 
             //to ask for a y value
+
 
             // compute the factorial of the number using a recursive function
             nAnswer = Power(nX, nY);//compile error - nY has no vallue assigned to it
             
             
-            Console.WriteLine("{nX}^{nY} = {nAnswer}");
+            //Console.WriteLine("{nX}^{nY} = {nAnswer}"); //Logic error - need to put $ in order for values to print
+            Console.WriteLine($"{nX}^{nY} = {nAnswer}");
         }
 
 
          //int Power(int nBase, int nExponent)
-        public static int Power(int nBase, int nExponent) //logic error - need to be public  static in so that main can access it
+        static int Power(int nBase, int nExponent) //logic error - need to be 5static in so that main can access it
         {
             int returnVal = 0;
             int nextVal = 0;
 
+            
             // the base case for exponents is 0 (x^0 = 1)
             if (nExponent == 0)
             {
                 // return the base case and do not recurse
-                returnVal = 0;
+                return 1;//logic error - if exponent is 0, the answer is 1
             }
             else
             {
                 // compute the subsequent values using nExponent-1 to eventually reach the base case
-                nextVal = Power(nBase, nExponent + 1);
+                //nextVal = Power(nBase, nExponent + 1); //logic error - want to be decrementing exponent, 
+                //not adding it, otherwise this will keep going forever
+                nextVal = Power(nBase, nExponent - 1);
 
                 // multiply the base with all subsequent values
                 returnVal = nBase * nextVal;
@@ -64,6 +69,10 @@ namespace UT1_BugSquash
 
             //returnVal; //compile error - need to return an int
             return returnVal;
-        }
-    }
-}
+            
+
+        }//power
+
+    }//program
+
+}//namespace
