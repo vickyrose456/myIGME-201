cd64cf38cd2c8fd335be22eb551bfa404e3ecf86
Victoria Olivieri <vro6068@rit.edu>
2021-02-26 22:44:23 -0500

    compile errors found

--- unitTestQ14/unitTestQ14/Program.cs
+++ unitTestQ14/unitTestQ14/Program.cs
@@ -9,31 +9,40 @@ namespace UT1_BugSquash
         {
             string sNumber;
             int nX;
-            int nY
+            //int nY --> forgot ; -compile error
+            int nY;
             int nAnswer;
 
-            Console.WriteLine(This program calculates x ^ y.);
+            //Console.WriteLine(This program calculates x ^ y.); //Needs "" - Compile error
+            Console.WriteLine("This program calculates x^y.");
 
             do
             {
                 Console.Write("Enter a whole number for x: ");
-                Console.ReadLine();
-            } while (!int.TryParse(sNumber, out nX));
+                sNumber = Console.ReadLine();
+            } //while (!int.TryParse(sNumber, out nX)); //compile error - sNumber has no value assigned to it
+            while (!int.TryParse(sNumber, out nX));
 
             do
             {
                 Console.Write("Enter a positive whole number for y: ");
-                sNumber = Console.ReadLine();
-            } while (int.TryParse(sNumber, out nX));
+                //sNumber = Console.ReadLine(); logic error - sNumber is already defined
+                //need to assign this input to nY(need to convert from string to int)
+                nY = Convert.ToInt32(Console.ReadLine());
+            } //while (int.TryParse(sNumber, out nX));
+            while (int.TryParse(sNumber, out nX));//runtime error - need to put !, otherwise it will continue 
+            //to ask for a y value
 
             // compute the factorial of the number using a recursive function
-            nAnswer = Power(nX, nY);
-
+            nAnswer = Power(nX, nY);//compile error - nY has no vallue assigned to it
+            
+            
             Console.WriteLine("{nX}^{nY} = {nAnswer}");
         }
 
 
-        int Power(int nBase, int nExponent)
+         //int Power(int nBase, int nExponent)
+        public static int Power(int nBase, int nExponent) //logic error - need to be public  static in so that main can access it
         {
             int returnVal = 0;
             int nextVal = 0;
@@ -53,7 +62,8 @@ namespace UT1_BugSquash
                 returnVal = nBase * nextVal;
             }
 
-            returnVal;
+            //returnVal; //compile error - need to return an int
+            return returnVal;
         }
     }
 }
