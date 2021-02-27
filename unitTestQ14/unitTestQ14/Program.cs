539f5f760cfdcf1bdd8c3007dfc89473e8cfea5a
Victoria Olivieri <vro6068@rit.edu>
2021-02-26 22:27:34 -0500

    copied Q14

---
+++ unitTestQ14/unitTestQ14/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace UT1_BugSquash
+{
+    class Program
+    {
+        // Calculate x^y for y > 0 using a recursive function
+        static void Main(string[] args)
+        {
+            string sNumber;
+            int nX;
+            int nY
+            int nAnswer;
+
+            Console.WriteLine(This program calculates x ^ y.);
+
+            do
+            {
+                Console.Write("Enter a whole number for x: ");
+                Console.ReadLine();
+            } while (!int.TryParse(sNumber, out nX));
+
+            do
+            {
+                Console.Write("Enter a positive whole number for y: ");
+                sNumber = Console.ReadLine();
+            } while (int.TryParse(sNumber, out nX));
+
+            // compute the factorial of the number using a recursive function
+            nAnswer = Power(nX, nY);
+
+            Console.WriteLine("{nX}^{nY} = {nAnswer}");
+        }
+
+
+        int Power(int nBase, int nExponent)
+        {
+            int returnVal = 0;
+            int nextVal = 0;
+
+            // the base case for exponents is 0 (x^0 = 1)
+            if (nExponent == 0)
+            {
+                // return the base case and do not recurse
+                returnVal = 0;
+            }
+            else
+            {
+                // compute the subsequent values using nExponent-1 to eventually reach the base case
+                nextVal = Power(nBase, nExponent + 1);
+
+                // multiply the base with all subsequent values
+                returnVal = nBase * nextVal;
+            }
+
+            returnVal;
+        }
+    }
+}
