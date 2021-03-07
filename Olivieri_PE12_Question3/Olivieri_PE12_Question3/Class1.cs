9573df796e4568ba3d19d5feb9bd076bbce18508
Victoria Olivieri <vro6068@rit.edu>
2021-03-06 22:14:07 -0500

    initial

---
+++ Olivieri_PE12_Question3/Olivieri_PE12_Question3/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace olivieri_PE12_Q3
+{
+    public abstract class MyClass
+    {
+        private string myString;
+
+        public string MyString
+        {
+            set
+            {
+                myString = value;
+            }
+        }//myString
+
+        public virtual string GetString()
+        {
+            return myString;
+        }
+    }//end my class
+
+    public class MyDerivedClass : MyClass
+    {
+        public override string GetString()
+        {
+
+            return GetString() + " output from derived class";
+        }
+    }
+
+
+}//end namespace
