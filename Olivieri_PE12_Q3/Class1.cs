aedf165cb1b0336927eeb56d8cb2b3092fd62837
Victoria Olivieri <vro6068@rit.edu>
2021-03-06 23:03:00 -0500

    Derived class w/ overriden method

---
+++ Olivieri_PE12_Q3/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using Olivieri_PE12_Q3;
+
+namespace Olivieri_Q3
+{
+    class Main_Method
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass dClass = new MyDerivedClass();
+            dClass.GetString();
+
+        }
+    }
+}
+
+   
