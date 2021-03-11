f2aa8b7ca7d83fdf4afa733053a5f14a96458a7b
Victoria Olivieri <vro6068@rit.edu>
2021-03-10 23:35:52 -0500

    main PE14

---
+++ Olivieri_PE14Main/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classy;
+
+namespace Main_PE14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare obj of each class
+            myAClass myClass = null;
+            my1stClass class1 = new my1stClass();
+            my2ndClass class2 = new my2ndClass();
+            myInterface myInterface = null;
+
+            myMethod(class1);
+            myMethod(class2);
+        }
+        //method called myMethod that passes in an object
+        public static void myMethod(myInterface myObject)
+        {
+            //print the cool Method for the obj
+            if (myObject.GetType() == typeof(my1stClass))
+            {
+                //use class 1 method
+                myObject.Cool();
+            }
+            else if (myObject.GetType() == typeof(my2ndClass))
+            {
+                //use class 2 method
+                myObject.Cool();
+            }
+
+
+        }
+
+
+    }
+
+
+}
