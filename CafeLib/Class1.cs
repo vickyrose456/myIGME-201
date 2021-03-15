a502ad10269511deb1b31f457cf9a08a6e1a8841
Victoria Olivieri <vro6068@rit.edu>
2021-03-15 01:00:12 -0400

    hotDrnk

---
+++ CafeLib/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeLib
+{
+    public abstract class HotDrink
+    {
+        public bool instant;
+        public bool milk;
+        private byte sugar;
+        public string size;
+        public Customer customer;
+
+        //AddSugar(amount:byte):v
+        public virtual int AddSugar(byte amount)
+        {
+            
+        }
+
+        //steam():a
+        public abstract void Steam()
+        {
+            
+        }
+
+        //()
+        public HotDrink() 
+        {
+
+        }
+
+        //(brand:strng)
+        public HotDrink(string brand)
+        {
+            
+        }
+        
+
+    }//abstract hotDrink
+
+
+
+
+
+}
