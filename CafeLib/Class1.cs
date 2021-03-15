fca904b0694e0e272b2dfea9a8715293f2b4a0d0
Victoria Olivieri <vro6068@rit.edu>
2021-03-15 01:49:56 -0400

    cupofcoffee

--- CafeLib/Class1.cs
+++ CafeLib/Class1.cs
@@ -4,8 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Victoria Olivieri
+//PE 16 - Classiest
+
 namespace CafeLib
 {
+    //+IMood
+    public interface IMood
+    {
+
+        //+Mood:string:r
+        public string Mood
+        {
+            get
+            {
+                return "";
+            }
+        }
+    }//IMood
+
+
+    //+Customer
+    public class Customer : HotDrink, IMood
+    {
+        //+name:string
+        public string name;
+        //+creditCardNum:string
+        public string creditCardNumber;
+
+        //Mood:String:r
+        public string Mood
+        {
+            get 
+            {
+                return "";
+            }
+        }
+    }//Customer
+
+    public class Waiter : IMood 
+    {
+        //+name:string
+        public string name;
+
+        //+mood:string:r
+        public string Mood
+        {
+            get 
+            {
+                return "";
+            }
+        }
+
+        //+serveCust(cup:hotDrink):void
+        public void ServeCustomer(HotDrink cup)
+        {
+            
+        }
+
+    }//Waiter
+
+
+
     public abstract class HotDrink
     {
         public bool instant;
@@ -15,16 +75,10 @@ namespace CafeLib
         public Customer customer;
 
         //AddSugar(amount:byte):v
-        public virtual int AddSugar(byte amount)
-        {
-            
-        }
+        public virtual int AddSugar(byte amount);
 
         //steam():a
-        public abstract void Steam()
-        {
-            
-        }
+        public abstract void Steam();
 
         //()
         public HotDrink() 
@@ -40,6 +94,40 @@ namespace CafeLib
         
 
     }//abstract hotDrink
+
+
+    //+CupofCoffee
+    public class CupOfCoffee : HotDrink
+    {
+        //beantype:string
+        public string beanType;
+
+        //+steam():o
+        public override void Steam ()
+        {
+
+        }
+
+        //+Takeorder()
+        public void TakeOrder()
+        {
+            
+        }
+
+        //+brand:string):base(brand)
+        public CupOfCoffee(string brand) : base(brand)
+        {
+            
+        }
+
+    }
+
+
+
+
+
+
+
 
 
 
