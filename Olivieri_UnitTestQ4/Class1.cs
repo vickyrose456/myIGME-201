2f297502f75551ebdf0261086d6fc0910b4aee51
Victoria Olivieri <vro6068@rit.edu>
2021-03-27 02:08:01 -0400

    Q4 Completed

---
+++ Olivieri_UnitTestQ4/Class1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olivieri_UnitTestQ4
+{
+    public abstract class Phone
+    {
+        private string phoneNumber;
+        public string address;
+
+        public string PhoneNumer
+        {
+            get;
+            set;
+        }
+
+        public abstract void Connect();
+        public abstract void Disconnect();
+    }//end phone
+
+    public interface IPhoneInterface
+    {
+        void Answer();
+        void MakeCall();
+        void HangUp();
+    }//end phone interface
+
+    public class RotaryPhone : Phone, IPhoneInterface
+    {
+        public void Answer()
+        { 
+        }
+        public void MakeCall()
+        {
+
+        }
+        public void HangUp()
+        { 
+
+        }
+        public override void Connect()
+        {
+      
+        }
+        public override void Disconnect()
+        {
+
+        }
+    }//end rotary phone
+
+    public class Tardis : RotaryPhone
+    {
+        private bool sonicScrewdriver;
+        private byte whichDrWho;
+        private string femaleSideKick;
+        public double exteriorSurfaceArea;
+        public double interiorVolume;
+
+        public byte WhichDrWho
+        {
+            get;
+        }
+
+        public string FemaleSideKick
+        {
+            get;
+        }
+
+        public void TimeTravel()
+        { 
+        }
+
+
+    }//end tardis
+
+
+
+    public class PushButtonPhone : Phone, IPhoneInterface
+    {
+        public void Answer()
+        {
+        }
+        public void MakeCall()
+        {
+
+        }
+        public void HangUp()
+        {
+
+        }
+        public override void Connect()
+        {
+
+        }
+        public override void Disconnect()
+        {
+
+        }
+    }//push button phone
+
+    public class PhoneBooth : PushButtonPhone
+    {
+        private bool superMan;
+        public double costPerCall;
+        public bool phoneBook;
+
+        public void OpenDoor()
+        {
+        }
+        public void CloseDoor() 
+        {
+        }
+    }
+
+
+}
--- workingExample/workingExample/Program.cs
+++ workingExample/workingExample/Program.cs
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 
 namespace workingExample
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+
+
+
 }
