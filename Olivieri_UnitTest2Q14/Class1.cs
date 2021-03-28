78ea0abb43fb0c26e1719d490f3453e399a06c5b
Victoria Olivieri <vro6068@rit.edu>
2021-03-28 01:50:53 -0400

    Q14 Completed

--- Olivieri_UnitTest2Q14/Class1.cs
+++ Olivieri_UnitTest2Q14/Class1.cs
@@ -26,42 +26,53 @@ namespace Olivieri_UnitTest2Q14
         }
     }
 
-   
-
-    /*
-
-    struct Friend
-    {
-        public string name;
-        public string greeting;
-        public DateTime birthdate;
-        public string address;
-    }
-
     class Program
     {
-
         static void Main(string[] args)
         {
-            Friend friend;
-            Friend enemy;
+            Friend friend = new Friend();
+            Enemy enemy = new Enemy();
 
-            // create my friend Charlie Sheen
-            friend.name = "Charlie Sheen";
-            friend.greeting = "Dear Charlie";
-            friend.birthdate = DateTime.Parse("1967-12-25");
-            friend.address = "123 Any Street, NY NY 12202";
-
-            // now he has become my enemy
-            enemy = friend;
-
-            // set the enemy greeting and address without changing the friend variable
-            enemy.greeting = "Sorry Charlie";
-            enemy.address = "Return to sender.  Address unknown.";
-
-            Console.WriteLine($"friend.greeting => enemy.greeting: {friend.greeting} => {enemy.greeting}");
+            Console.WriteLine(($"friend.greeting => enemy.greeting: {friend.greeting} => {enemy.greeting}"));
             Console.WriteLine($"friend.address => enemy.address: {friend.address} => {enemy.address}");
         }
     }
-    */
-}
+    
+
+        /*
+
+        struct Friend
+        {
+            public string name;
+            public string greeting;
+            public DateTime birthdate;
+            public string address;
+        }
+
+        class Program
+        {
+
+            static void Main(string[] args)
+            {
+                Friend friend;
+                Friend enemy;
+
+                // create my friend Charlie Sheen
+                friend.name = "Charlie Sheen";
+                friend.greeting = "Dear Charlie";
+                friend.birthdate = DateTime.Parse("1967-12-25");
+                friend.address = "123 Any Street, NY NY 12202";
+
+                // now he has become my enemy
+                enemy = friend;
+
+                // set the enemy greeting and address without changing the friend variable
+                enemy.greeting = "Sorry Charlie";
+                enemy.address = "Return to sender.  Address unknown.";
+
+                Console.WriteLine($"friend.greeting => enemy.greeting: {friend.greeting} => {enemy.greeting}");
+                Console.WriteLine($"friend.address => enemy.address: {friend.address} => {enemy.address}");
+            }
+        }
+        */
+    }
