064622a88bc27a3ce6afb96ed1a16b0ea83e2b71
Victoria Olivieri <vro6068@rit.edu>
2021-04-12 03:12:26 -0400

    Search form cs

--- GIFFinder/Form2.cs
+++ GIFFinder/Form2.cs
@@ -12,9 +12,41 @@ namespace GIFFinder
 {
     public partial class searchForm : Form
     {
+        public string response;
+        public string searchTerms;
+        public int maxItems;
+
         public searchForm()
         {
+            
             InitializeComponent();
+
+            this.maxItemsTextBox.KeyPress += new KeyPressEventHandler(MaxItemsTextBox__KeyPressed);
+            this.cancelButton.Click += new EventHandler(CancelButton__Click);
+            this.okButton.Click += new EventHandler(OkButton__Click);
+
+        }
+        private void MaxItemsTextBox__KeyPressed(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            if (Char.IsDigit(e.KeyChar) || e.KeyChar == '\b')
+            {
+                e.Handled = false;
+            }
+        }
+        private void CancelButton__Click(object sender, EventArgs e)
+        {
+            this.response = "Cancel";
+
+            this.Hide();
+        }
+        private void OkButton__Click(object sender, EventArgs e)
+        {
+            this.response = "OK";
+
+            this.searchTerms = searchTermTextBox.Text;
+            this.maxItems = Convert.ToInt32(maxItemsTextBox.Text);
+            this.Hide();
         }
 
         private void searchTermlabel_Click(object sender, EventArgs e)
