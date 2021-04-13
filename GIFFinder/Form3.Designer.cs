a7d5f21147656f7a67f6d2ce9b7085a786b05645
Victoria Olivieri <vro6068@rit.edu>
2021-04-12 22:14:39 -0400

    shows img

--- GIFFinder/Form3.Designer.cs
+++ GIFFinder/Form3.Designer.cs
@@ -29,19 +29,39 @@ namespace GIFFinder
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(28, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(298, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // ImageForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(1273, 585);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "ImageForm";
             this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
     }
 }
--- GIFFinder/Form3.cs
+++ GIFFinder/Form3.cs
@@ -7,14 +7,82 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace GIFFinder
 {
     public partial class ImageForm : System.Windows.Forms.Form
     {
-        public ImageForm()
+        string title;
+        public ImageForm(GIFFinder parent, string url, string title)
         {
             InitializeComponent();
+
+            this.MdiParent = parent;
+            this.Text = title;
+            this.title = title;
+
+            parent.closeAllToolStripMenuItem.Click += new EventHandler(CloseAllToolStripMenuItem__Click);
+            parent.saveToolStripMenuItem.Click += new EventHandler(SaveToolStripMenuItem__Click);
+
+            this.FormClosing += new FormClosingEventHandler(ImageForm__FormClosing);
+            
+            pictureBox1.ImageLocation = url;
         }
+
+        private void CloseAllToolStripMenuItem__Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ImageForm__FormClosing(object sender, FormClosingEventHandler e)
+        {
+            GIFFinder parent = (GIFFinder)this.MdiParent;
+
+            parent.closeAllToolStripMenuItem.Click -= new EventHandler(CloseAllToolStripMenuItem__Click);
+            parent.saveToolStripMenuItem.Click -= new EventHandler(SaveToolStripMenuItem__Click);
+        }
+
+        private void SaveToolStripMenuItem__Click(object sender, EventArgs e)
+        {
+            if (this != this.MdiParent.ActiveMdiChild)
+            {
+                return;
+            }
+            saveFileDialog.Filter = "Bmp(*.BMP)|*.BMP|Jpg(*.JPG)|*.JPG|Png(*.PNG)|*.PNG|Gif(*.GIF)|*.GIF";
+            saveFileDialog.FileName = this.title;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                switch (Path.GetExtension(saveFileDialog.FileName).ToUpper())
+                {
+                    case ".BMP":
+                        {
+                            pictureBox1.Image.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                            break;
+                        }
+                    case ".PNG":
+                        {
+                            pictureBox1.Image.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                            break;
+                        }
+                    case ".JPG":
+                    case ".JPEG":
+                        {
+                            pictureBox1.Image.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                            break;
+                        }
+                    case ".GIF":
+                        {
+                            pictureBox1.Image.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Gif);
+                            break;
+                        }
+                }
+                    
+                    
+            }
+        }
+
+
     }
 }
