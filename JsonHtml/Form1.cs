4ce661b4253fab1b116f44dae061080b98ea7c16
Victoria Olivieri <vro6068@rit.edu>
2021-04-18 01:46:08 -0400

    key value pairs for student and teachers

--- JsonHtml/Form1.cs
+++ JsonHtml/Form1.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Newtonsoft.Json;
+using PeopleAppGlobals;
+using PeopleLib;
+using System.IO;
+
 
 namespace JsonHtml
 {
@@ -16,6 +20,31 @@ namespace JsonHtml
         public Form1()
         {
             InitializeComponent();
+
+            Globals.AddPeopleSampleData();
+
+            //serialized that sorted list.. Need to do student/teah indiv
+            List<Teacher> teachers = new List<Teacher>();
+            List<Student> students = new List<Student>();
+
+            foreach (KeyValuePair<string, Person> keyValuePair in Globals.people.sortedList)
+            {
+                if (keyValuePair.Value.GetType() == typeof(Teacher))
+                {
+                    teachers.Add((Teacher)keyValuePair.Value);
+                }
+                else 
+                {
+                    students.Add((Student)keyValuePair.Value);
+                }
+            }
+
+            //JSON will convert it to readible data
+            string s = JsonConvert.SerializeObject(students);
+            string t = JsonConvert.SerializeObject(teachers);
+
+            //now write them to a file
+
         }
     }
 }
