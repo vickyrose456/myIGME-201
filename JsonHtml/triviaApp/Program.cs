75855a01cc683170d05344995819c57de232ac6f
Victoria Olivieri <vro6068@rit.edu>
2021-04-18 20:35:30 -0400

    Completed video tutorial. Recieivng data from JSon

--- JsonHtml/triviaApp/Program.cs
+++ JsonHtml/triviaApp/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.IO;
 using System.Net;
+using System.Web;
 
 namespace triviaApp
 {
@@ -26,7 +27,7 @@ namespace triviaApp
     {
         public int response_code;
         //list of results
-        public List<TriviaResult> TriviaResult;
+        public List<TriviaResult> results;
 
     }
    
@@ -34,6 +35,34 @@ namespace triviaApp
     {
         static void Main(string[] args)
         {
+            string url = null;
+            string s = null;
+
+            HttpWebRequest request;
+            HttpWebResponse response;
+            StreamReader reader;
+
+            url = "https://opentdb.com/api.php?amount=1";
+
+            request = (HttpWebRequest)WebRequest.Create(url);
+            //rends request to the url
+            response = (HttpWebResponse)request.GetResponse();
+
+            reader = new StreamReader(response.GetResponseStream());
+
+            s = reader.ReadToEnd();
+            reader.Close();
+
+            //deserialize into trivia obj
+            Trivia trivia = JsonConvert.DeserializeObject<Trivia>(s);
+
+            trivia.results[0].question = HttpUtility.HtmlDecode(trivia.results[0].question);
+            trivia.results[0].correct_answer = HttpUtility.HtmlDecode(trivia.results[0].correct_answer);
+
+            for (int i=0; i< trivia.results[0].incorrect_answers.Count; i++)
+            {
+                trivia.results[0].incorrect_answers[i] = HttpUtility.HtmlDecode(trivia.results[0].incorrect_answers[i]);
+            }
         }
     }
 }
