187c8e47b015318ede35886f1dfe1812e6b9d1c1
Victoria Olivieri <vro6068@rit.edu>
2021-05-13 02:12:45 -0400

    added password string to Person

--- TigerChatPeopleLib/Class1.cs
+++ TigerChatPeopleLib/Class1.cs
@@ -5,108 +5,232 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TigerChatPeopleLib
-{
-    public enum genderPronoun
     {
-        him,
-        her,
-        them
+        public enum genderPronoun
+        {
+            him,
+            her,
+            them
+        }
+        public enum collegeYear : byte
+        {
+            freshman = 23,
+            sophomore = 22,
+            junior = 21,
+            senior = 20
+        }
+
+        public interface ICourseList
+        {
+            List<string> CourseList
+            {
+                get;
+                set;
+            }
+        }
+
+        public abstract class Person
+        {
+            public string userName;
+            public int age;
+            public genderPronoun eGender;
+            public string email;
+
+            public string photoPath;
+            public string homePageURL;
+            public DateTime dateOfBirth;
+
+
+            private string password;
+            public bool resetPassword;
+            
+            public string Password
+            {
+                get
+                {
+                    return password;
+                }
+                set 
+                {
+                    //allow user to reset password
+                    if (resetPassword == true)
+                    {
+                        password = value;
+                    }
+                }
+            }
+
+            public static bool operator <(Person p1, Person p2)
+            {
+                return (p1.age < p2.age);
+            }
+
+            public static bool operator >(Person p1, Person p2)
+            {
+                return (p1.age > p2.age);
+            }
+
+            public static bool operator <=(Person p1, Person p2)
+            {
+                return (p1.age <= p2.age);
+            }
+
+            public static bool operator >=(Person p1, Person p2)
+            {
+                return (p1.age >= p2.age);
+            }
+
+            public static bool operator ==(Person p1, Person p2)
+            {
+                return (p1.age == p2.age);
+            }
+
+            public static bool operator !=(Person p1, Person p2)
+            {
+                return (p1.age != p2.age);
+            }
+
+        }
+
+        public interface IStudent
+        {
+            
+        }
+
+        public interface IPerson
+        {
+        }
+
+        public class Student : Person, IPerson, IStudent, ICourseList
+        {
+            public double gpa;
+            public collegeYear eCollegeYear;
+            public List<string> courseCodes = new List<string>();
+
+            public string favoriteFood;
+
+            public List<String> CourseList
+            {
+                get
+                {
+                    return this.courseCodes;
+                }
+
+                set
+                {
+                    this.courseCodes = value;
+                }
+            }
+
+            public static bool operator <(Student s1, Student s2)
+            {
+                return (s1.gpa < s2.gpa);
+            }
+
+            public static bool operator <=(Student s1, Student s2)
+            {
+                return (s1.gpa <= s2.gpa);
+            }
+
+            public static bool operator >(Student s1, Student s2)
+            {
+                return (s1.gpa > s2.gpa);
+            }
+
+            public static bool operator >=(Student s1, Student s2)
+            {
+                return (s1.gpa >= s2.gpa);
+            }
+
+            public static bool operator ==(Student s1, Student s2)
+            {
+                return (s1.gpa == s2.gpa);
+            }
+
+            public static bool operator !=(Student s1, Student s2)
+            {
+                return (s1.gpa != s2.gpa);
+            }
+
+
+        }
+
+        public class Teacher : Person, IPerson, ICourseList
+        {
+            public string specialty;
+            public List<string> courseCodes = new List<string>();
+
+            public List<String> CourseList
+            {
+                get
+                {
+                    return this.courseCodes;
+                }
+
+                set
+                {
+                    this.courseCodes = value;
+                }
+            }
+
+        }
+
+        public class People
+        {
+            // the generic SortedList class uses a template <> to store indexed data
+            // the first type is the data type to index on
+            // the second type is the data type to store in the list
+            public SortedList<string, Person> sortedList = new SortedList<string, Person>();
+
+            public void Remove(string userName)
+            {
+                if (userName != null)
+                {
+                    sortedList.Remove(userName);
+                }
+            }
+
+            // indexer property allows array access to sortedList via the class object
+            // and catching missing keys and duplicate key exceptions 
+            // notice the indexer property definition shows how it will be used in the calling code:
+            // if we have:
+            //     People people;
+            // then we can call:
+            //     people[email] to access the Person object with that email address
+            // and value will be the Person object (person) being added to the list in the case of:
+            //     people[email] = person;
+            public Person this[string userName]
+            {
+                get
+                {
+                    Person returnVal;
+                    try
+                    {
+                        returnVal = (Person)sortedList[userName];
+                    }
+                    catch
+                    {
+                        returnVal = null;
+                    }
+
+                    return (returnVal);
+                }
+
+                set
+                {
+                    try
+                    {
+                        // we can add to the list using these 2 methods
+                        //      sortedList.Add(email, value);
+                        sortedList[userName] = value;
+                    }
+                    catch
+                    {
+                        // an exception will be raised if an entry with a duplicate key is added
+                        // duplicate key handling (currently ignoring any exceptions)
+                    }
+                }
+            }
+        }
+
     }
-    
-    public abstract class Person
-    {
-        public string userName;
-        //user email address
-        public string email;
-        //for the user photo
-        public string photoPath;
-        //the user's password
-        private string userPassword;
-        //bool for if the user wants to change password
-        public bool resetPassword;
-        //gender pronoun
-        public genderPronoun userGender;
-        //the user's b-day
-        public DateTime dateOfBirth;
-        public string UserPassword 
-        {
-            get 
-            {
-                return userPassword;
-            }
-            set 
-            {
-                //set the password if the user clicks "reset password"
-                if (resetPassword == true)
-                {
-                    userPassword = value;
-                }
-            }
-        }
-
-    }//public class person 
-
-    public interface IUser
-    {
-        void Settings();
-    }
-
-
-    public class User : Person
-    {
-        public List<String> friendsNames = new List<string>();
-
-        public List<String> FriendsList
-        {
-            get 
-            {
-                return this.friendsNames;
-            }
-            set 
-            {
-                this.friendsNames = value;
-            }
-        }
-    }//user: peron
-
-    public class People
-    {
-        //store people objects into the sorted list, want person object and password
-        public SortedList<string,  Person> myList = new SortedList<string, Person>();
-        
-        public void Remove(string name)
-        {
-            if (name != null)
-            {
-                myList.Remove(name);
-            }
-        }
-
-        public Person this[string userName]
-        {
-            get 
-            {
-                Person returnVal;
-                try
-                {
-                    returnVal = (Person)myList[userName];
-                }
-                catch 
-                {
-                    returnVal = null;
-                }
-                return (returnVal);
-            }
-            set 
-            {
-                try
-                {
-                    //add to the list
-                    myList.Add(userName, value);
-                }
-                catch {//duplicate handling 
-                }
-            }
-        }
-    }
-}
